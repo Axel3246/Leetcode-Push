ce05a080b624861ddb8773ee324ebbd23fd78ba7
Axel Hernández <hplaxel@gmail.com>
2025-02-13 10:11:20 -0600

    Time: 7 ms (81.6%), Space: 31.7 MB (22.37%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -6,12 +6,18 @@ class Solution:
 
         # Fill the set with the contents of nums
         for i in nums:
+            # 2nd way: iteratively fill the set, and when we encounter a duplicate, return true
+            if i in myset:
+                return True
             myset.add(i)
+        
+        return False
 
-        # Now, if the set is the same length as nums, it means there are no duplicates of a digit, hence returning False. On the other hand, if its not the same length, it means there is at least 1 digit that is duplicated, hence returning True.
+'''Now, if the set is the same length as nums, it means there are no duplicates of a digit, hence returning False. On the other hand, if its not the same length, it means there is at least 1 digit that is duplicated, hence returning True.
 
         if(len(myset) == len(nums)):
             return False
         return True
         
+'''
         
