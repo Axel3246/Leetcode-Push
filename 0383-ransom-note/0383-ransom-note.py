2c91281c36968e895885f1e91c39c8a66cea54eb
Axel Hernández <hplaxel@gmail.com>
2025-02-17 11:20:24 -0600

    Time: 28 ms (17.39%), Space: 17.8 MB (96.47%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,14 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
+        # Create a hashmap
         hmap = {}
 
+        # Count the chars in the magazine
         for char in magazine:
             hmap[char] = hmap.get(char, 0) + 1
         
+        # Check if we can construct the ransom letters with the counted ones
         for char in ransomNote:
             if char not in hmap:
                 return False
