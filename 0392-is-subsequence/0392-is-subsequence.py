99ae532e2a9a09e9d31d57270ff542949d78deef
Axel Hernández <hplaxel@gmail.com>
2025-03-10 22:21:23 -0600

    Time: 0 ms (100%), Space: 17.8 MB (68.24%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,16 +1,21 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         
+        # Start two pointers
         L, R = 0,  0
 
+        # Iterate until we reach the end of the shorter string (s)
         while L != len(s):
+
+            # If we are at the end of the largest string, theres no subseq
             if R == len(t):
                 return False
             
+            # If we match, advance the two pointers
             if s[L] == t[R]:
                 R += 1
                 L += 1
-            else:
+            else: # Else just advance the one of the largest
                 R += 1
         
         return True
