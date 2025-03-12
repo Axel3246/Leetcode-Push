83cb437526193b864a56e45dd867f6f5ce26cae1
Axel Hernández <hplaxel@gmail.com>
2025-03-11 21:46:35 -0600

    Time: 1 ms (59.59%), Space: 18.7 MB (65.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,6 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
         hmap = {}
 
         # We traverse through idx and content
