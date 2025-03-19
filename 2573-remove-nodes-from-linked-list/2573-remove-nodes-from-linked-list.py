b962ef9662b675021c92da76ca63d12f48d62a96
Axel Hernández <hplaxel@gmail.com>
2025-03-19 08:57:51 -0600

    Time: 170 ms (21.18%), Space: 58.4 MB (32.53%) - LeetHub

--- 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/2573-remove-nodes-from-linked-list.py
@@ -6,27 +6,32 @@
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        # 1. Use a Monotonic Stack to keep track of the 'greater' values
         stack = [head.val]
         head = head.next
-        
 
+        # 2. Traverse the Linked List and check for the following
         while head:
+            # 2.1 If we find a greater value, we need to modify our stack until the value either
+            # the first value in the stack or it's not the greatest anymore
             if head.val > stack[-1]:
                 while stack and stack[-1] < head.val:
                     stack.pop()
                 stack.append(head.val)
-            else:
+            else: # 2.2 If we find something else, we just append
                 stack.append(head.val)
 
             head = head.next
 
-        head = currNode = None
+        # 3. Create a dummyHead and a new pointer to reconstruct the list from the stack
+        dummy = ListNode(0)
+        currNode = dummy
+
+        # 4. Iterate through the stack and construct the new LL
         for node in stack:
-            if not head:
-                currNode = ListNode(node)
-                head = currNode
-            else:
-                currNode.next = ListNode(node)
-                currNode = currNode.next
+            currNode.next = ListNode(node)
+            currNode = currNode.next
         
+        # 5. Simply return the node next to dummy
+        return dummy.next
         return head
