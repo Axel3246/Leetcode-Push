6f99ce851ba93d3dbd73f141f7d0c4a9bf80a004
Axel Hernández <hplaxel@gmail.com>
2025-03-29 00:28:05 -0600

    Time: 452 ms (15.01%), Space: 17.9 MB (40.19%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
@@ -1,16 +1,20 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
         
-        
+        # 1. CREATE A HELPER VAR WHICH WILL HOLD THE SUM
         helper = 1 
 
+        # 2. ITER THROUGH THE SERIES
         for i in range(1, n+1):
             
+            # 3. GET THE VALUE FROM I THROUGH N (N+1 TO INCLUDE N)
             b = sum(range(i, n+1))
 
+            # 4. CHECK IF BOTH SUMS MATCH, IF THEY DO RETURN THE PIVOT INT
             if helper == b:
                 return i
             
+            # 5. ELSE WE UPDATE OUR SUM
             helper += i+1
-
+        
         return -1
