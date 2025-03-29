fb43fca84371069a96700e64e7fc64332597bbea
Axel Hernández <hplaxel@gmail.com>
2025-03-29 00:26:22 -0600

    Time: 448 ms (15.01%), Space: 17.8 MB (60.44%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
@@ -1,15 +1,16 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
         
-        helper = [1] 
+        
+        helper = 1 
 
         for i in range(1, n+1):
             
             b = sum(range(i, n+1))
-            print(helper, b)
-            if helper[0] == b:
+
+            if helper == b:
                 return i
             
-            helper[0] += i+1
+            helper += i+1
 
         return -1
