e99ff961cb380b302d6b49608f2552ab268f88db
Axel Hernández <hplaxel@gmail.com>
2025-03-31 22:18:35 -0600

    Time: 31 ms (92.09%), Space: 17.7 MB (48.77%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -4,12 +4,14 @@ class Solution:
         L, R = 0, 0
         ans = ''
         
+        # 2. Append the characters alternatively
         while R != len(word2) and L != len(word1):
             ans += word1[L]
             ans += word2[R]
             L += 1
             R += 1
 
+        # 3. Append the rest if nedeed. If idx is OOB, it doesnt append anything
         if R == len(word2):
             return ans + word1[L:]
         return ans + word2[R:]
