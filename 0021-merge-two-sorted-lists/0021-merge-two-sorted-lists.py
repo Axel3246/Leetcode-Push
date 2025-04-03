adddd8764b78d2ff8987abeb893805a81f83e74f
Axel Hernández <hplaxel@gmail.com>
2025-04-03 08:39:55 -0600

    Time: 0 ms (100%), Space: 18 MB (18.69%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,18 +6,26 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         
+        # 1. Create a two pointers - a dummy head and a node to iterate
         dummyNode = ListNode(0)
         currNode = dummyNode
 
+        # 2. While we don't end up in a None in either list
         while list1 and list2:
+            # 3. If the value of the first list is smaller or equal to the value of the second
+            # set currNode.next to list1 and advance list1
             if list1.val <= list2.val:
                 currNode.next = list1
                 list1 = list1.next
+            # 4. Else, we find out that the list2.val is smaller, so we set currNode.next to it
+            # and advance list2
             else:
                 currNode.next = list2
                 list2 = list2.next
             currNode = currNode.next
         
+        # 5. If we don't have the same length of lists, there will be missing nodes,
+        # so we just check
         if list1:
             currNode.next = list1
         if list2:
