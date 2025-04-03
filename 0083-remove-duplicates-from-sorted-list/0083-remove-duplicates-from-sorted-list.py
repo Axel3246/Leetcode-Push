577573eac7985ac3bdf36aeab5ff02f32108e472
Axel Hernández <hplaxel@gmail.com>
2025-04-03 09:48:08 -0600

    Time: 2 ms (28.32%), Space: 17.7 MB (65.88%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -6,17 +6,23 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        # 1. If theres no head, then return head
         if not head:
             return head
 
+        # 2. Create two nodes to relink nodes. Almost like a slow-fast approach
         currNode, helperNode = head, head.next
 
+        # 3. While our fast node exists
         while helperNode:
+            # 4. If we find that our val is not the same, we relink the nodes
             if currNode.val != helperNode.val:
                 currNode.next = helperNode
                 currNode = helperNode
+            # 5. Else, we just advance the helper node
             helperNode = helperNode.next
         
+        # 6. We link the last node possible node to helperNode, which should be None
         currNode.next = helperNode
 
         return head
