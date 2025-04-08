a71a552570d95d853ed55ad835e03ec825b6768f
Axel Hernández <hplaxel@gmail.com>
2025-04-07 23:16:42 -0600

    Time: 3 ms (32.77%), Space: 18.1 MB (24.19%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -4,11 +4,10 @@ class Solution:
         L = len(s)-1
         ans = ''
         while L > -1:
-            
             if s[L] != ' ':
                 R = L
                 while s[L] != ' ' and L != -1:
-                    print(s[L], L)
+                    #print(s[L], L)
                     L -= 1
                 ans = ans + s[L+1:R+1] + ' '
             else:
